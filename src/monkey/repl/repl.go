5bd46a3f0516cf0fb49a3efc2b2d4d88682de390
rishavmngo <rishavinmngo@gmail.com>
2023-01-25 19:53:04 +0530

    till lexing

---
+++ src/monkey/repl/repl.go
@@ -0,0 +1,32 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+		line := scanner.Text()
+
+			if(line == ".exit") {
+			fmt.Println("Thank You for using Monkey REPL!")
+				break
+			}
+		l := lexer.New(line)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
